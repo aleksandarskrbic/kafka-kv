ac110cbdef6c5dc128f138d7fb135764b0714b11
aleksandarskrbic <skrbic.alexa@gmail.com>
2022-02-18 09:08:21 +0100

    Implement topic deletion test

--- modules/kafka-admin/src/test/scala/kafka/kv/admin/KafkaAdminSpec.scala
+++ modules/kafka-admin/src/test/scala/kafka/kv/admin/KafkaAdminSpec.scala
@@ -7,7 +7,7 @@ import org.scalatest.matchers.should.Matchers._
 class KafkaAdminSpec extends KafkaSpec {
 
   it should "create kafka topic" in {
-    val testTopic = "test-topic"
+    val testTopic = "test.topic"
 
     kafkaAdmin
       .createCompactedTopic(testTopic)
@@ -25,6 +25,29 @@ class KafkaAdminSpec extends KafkaSpec {
         }.get
       }
       .map(topics => topics should contain(testTopic))
+  }
+
+  it should "delete kafka topic" in {
+    val topicToDelete = "topic.to.delete"
+
+    createCustomTopic(topicToDelete)
+
+    kafkaAdmin
+      .deleteTopic(topicToDelete)
+      .flatMap { _ =>
+        withAdminClient { adminClient =>
+          val promise = Promise[Set[String]]()
+          val kafkaFuture = adminClient.listTopics().names()
+
+          kafkaFuture.whenComplete { (result, error) =>
+            if (error != null) promise.failure(error)
+            else promise.success(result.asScala.toSet)
+          }
+
+          promise.future
+        }.get
+      }
+      .map(topics => topics should not contain topicToDelete)
   }
 
 }
